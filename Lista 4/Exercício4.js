b8f29162e6cd0f9616ab77c80d3103ab2118e7cb
Maria Luiza Santos da Cruz <lulucruz255@gmail.com>
2025-10-01 20:11:50 -0300

    Create Exercício4.js

---
+++ Lista 4/Exercício4.js
@@ -0,0 +1,32 @@
+const input = require("readline-sync");
+
+let numero1 = Number( input.question("Digite o primeiro número: ") )
+let numero2 = Number( input.question("Digite o segundo número: ") )
+let numero3 = Number( input.question("Digite o segundo número: ") )
+
+if (numero1 > numero2 && numero1 > numero3) {
+    console.log(numero1)
+}
+else if (numero2 > numero3) {
+    console.log(numero2)
+}
+else {
+    console.log(numero3)
+}
+
+// QUAL A DIFERENÇA?
+
+if (numero1 > numero2) { 
+    if (numero1 > numero3) {
+       console.log(numero1)
+    } else {
+        console.log(numero3)
+    }
+}
+else {
+    if (numero2 > numero3) {
+    console.log(numero2)
+    } else {
+    console.log(numero3)
+    }
+}
