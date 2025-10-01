374103eab0107a2ca7bf8959767f173c81b6c3fe
Maria Luiza Santos da Cruz <lulucruz255@gmail.com>
2025-10-01 20:10:52 -0300

    Create Exercício3.js

---
+++ Lista 4/Exercício3.js
@@ -0,0 +1,9 @@
+const input = require("readline-sync");
+
+let numero = Number( input.question("Digite um número: "))
+
+if (numero > 10) {
+    console.log("Número maior que 10")
+} else {
+    console.log("Número menor ou igual a 10")
+}
