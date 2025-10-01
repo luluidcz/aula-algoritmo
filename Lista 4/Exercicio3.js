a77158bd769c9bf9046c2017f0736d2004cdaa85
Maria Luiza Santos da Cruz <lulucruz255@gmail.com>
2025-10-01 20:08:51 -0300

    Delete Lista 4/Exercicio3.js

--- Lista 4/Exercicio3.js
+++
@@ -1,9 +0,0 @@
-const input = require("readline-sync");
-
-let numero = Number( input.question("Digite um número: ") )
-
-if (numero > 10) {
-    console.log("Número maior que 10")
-} else {
-    console.log("Número menor ou igual a 10")
-}
