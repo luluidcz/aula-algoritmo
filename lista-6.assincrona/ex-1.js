68f0ab29c978cfc4ef83365ab48ed97744dd20f0
Maria Luiza Santos da Cruz <lulucruz255@gmail.com>
2025-11-26 21:44:05 -0300

    Add simple counting from 1 to 20

---
+++ lista-6.assincrona/ex-1.js
@@ -0,0 +1,8 @@
+const input = require("readline-sync");
+
+//1️⃣ Contagem Simples Mostre todos os números de 1 até 20.
+console.log("mostre todos os números de 1 até 20");
+for(let numero =1; numero <=20; numero++) {
+    console.log("o numero = ",numero);
+}
+
